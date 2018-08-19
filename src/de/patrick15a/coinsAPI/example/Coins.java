15cecd116b1489c0199195709e0d9deb8ec24457
Patrick15a <gamer.patrick15@gmx.de>
2018-08-19 15:36:43 +0200

    Update Coins.java

--- src/de/patrick15a/coinsAPI/example/Coins.java
+++ src/de/patrick15a/coinsAPI/example/Coins.java
@@ -3,21 +3,30 @@ package de.patrick15a.coinsAPI.example;
 import org.bukkit.entity.Player;
 
 public class Coins {
-
-	public void get(Player p) {
-		Main.getCoinsAPI().getCoins(p);
+	
+	// Gibt zurück wie viele Coins der Spieler hat.
+	public int get(Player p) {
+		return Main.getCoinsAPI().getCoins(p);
 	}
 	
-	public void add(Player p, int coins) {
-		Main.getCoinsAPI().addCoins(p, coins);
+	// Fügt dem Spieler Coins hinzu. Gibt zurück ob Funktion erfolgreich ausgeführt wurde.
+	public boolean add(Player p, int coins) {
+		return Main.getCoinsAPI().addCoins(p, coins);
 	}
 	
-	public void remove(Player p, int coins) {
-		Main.getCoinsAPI().removeCoins(p, coins);
+	// Entfernt dem Spieler Coins. Gibt zurück ob Funktion erfolgreich ausgeführt wurde.
+	public boolean remove(Player p, int coins) {
+		return Main.getCoinsAPI().removeCoins(p, coins);
 	}
 	
-	public void set(Player p, int coins) {
-		Main.getCoinsAPI().setCoins(p, coins);
+	// Setzt dem Spieler Coins. Gibt zurück ob Funktion erfolgreich ausgeführt wurde.
+	public boolean set(Player p, int coins) {
+		return Main.getCoinsAPI().setCoins(p, coins);
+	}
+	
+	// Gibt zurück ob der Spieler "amount" Coins hat.
+	public boolean has(Player p, int coins) {
+		return Main.getCoinsAPI().hasCoins(p, coins);
 	}
 	
 }
